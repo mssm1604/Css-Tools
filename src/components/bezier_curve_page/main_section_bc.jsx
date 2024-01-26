48ae07a2186a92d971f8d3f720c3242a01436171
mssm1604 <smonsalve386@gmail.com>
2024-01-26 18:28:25 -0500

    refactor(main section bc)

--- src/components/bezier_curve_page/main_section_bc.jsx
+++ src/components/bezier_curve_page/main_section_bc.jsx
@@ -1,76 +1,19 @@
-import { useState } from 'react'
 import { CheckIcon, ClipboardIcon, RightArrow } from '../icons/icons'
 import { Canvas } from './canvas/canvas'
-import './main_section_bc.css'
 import { DemoSlider } from '../demos/slider'
+import { useMainSectionBc } from '../../hooks/useMainSectionBc'
+import './main_section_bc.css'
 
 export function MainSectionBc() {
-  const [controlPoints, setControlPoints] = useState({
-    cp1: {
-      x: 180,
-      y: 290
-    },
-    cp2: {
-      x: 122,
-      y: 100
-    }
-  })
-  const [parsedParams, setParsedParams] = useState({
-    cp1: { x: 0.65, y: 0.05 },
-    cp2: { x: 0.36, y: 1 }
-  })
-
-  const cubicBezierValue = `cubic-bezier(
-    ${parsedParams.cp1.x},
-    ${parsedParams.cp1.y},
-    ${parsedParams.cp2.x},
-    ${parsedParams.cp2.y})`
-
-  const handleInputOnChange = e => {
-    const inputName = e.target.name
-    const inputAxis = e.target.getAttribute('data-axis')
-    const inputValue = e.target.value
-
-    if (
-      (inputAxis === 'x' && inputValue >= 1) ||
-      (inputAxis === 'x' && inputValue <= 0)
-    ) {
-      setParsedParams(prevVale => ({
-        ...prevVale,
-        [inputName]: {
-          x: inputValue > 0 ? 1.0 : 0.0,
-          y: prevVale[inputName].y
-        }
-      }))
-      return
-    }
-
-    const parsedInputValue =
-      inputAxis === 'x' ? inputValue * 200 + 50 : 400 - (inputValue * 200 + 100)
-
-    setControlPoints(prevValue => ({
-      ...prevValue,
-      [inputName]: {
-        x: inputAxis === 'x' ? parsedInputValue : prevValue[inputName].x,
-        y: inputAxis === 'y' ? parsedInputValue : prevValue[inputName].y
-      }
-    }))
-  }
-
-  const handleResultsFieldOnClick = async () => {
-    const copiedMessage = document.querySelector('.results_tooltip')
-
-    try {
-      await navigator.clipboard.writeText(cubicBezierValue)
-      copiedMessage.classList.add('active')
-
-      setTimeout(() => {
-        copiedMessage.classList.remove('active')
-      }, 800)
-    } catch (error) {
-      console.error('there was an error trying to copy this', error)
-    }
-  }
+  const {
+    controlPoints,
+    setControlPoints,
+    parsedParams,
+    setParsedParams,
+    cubicBezierValue,
+    handleInputOnChange,
+    handleResultsFieldOnClick
+  } = useMainSectionBc()
 
   return (
     <main>
